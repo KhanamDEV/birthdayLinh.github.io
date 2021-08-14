54a7ebfe63a40dd12fc2bbf729a62f8359c82bd6
Khả Nam <60590394+khanamtechasia@users.noreply.github.com>
2021-08-14 19:59:41 +0700

    ud

--- main.js
+++ main.js
@@ -386,10 +386,10 @@ function animate() {
     controls.update();
     // composer.render();
 }
-document.addEventListener('keydown', function(event){
-    if (event.keyCode == 123) { // Prevent F12
-        return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-        return false;
-    }
-});
+// document.addEventListener('keydown', function(event){
+//     if (event.keyCode == 123) { // Prevent F12
+//         return false;
+//     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+//         return false;
+//     }
+// });
